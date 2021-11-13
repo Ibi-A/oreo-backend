6b52a571e2e232f1da21b521ba9be59a406893a5
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-13 20:42:13 +0100

    Initial commit

---
+++ test/oreo-backend.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as OreoBackend from '../lib/oreo-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/oreo-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OreoBackend.OreoBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
